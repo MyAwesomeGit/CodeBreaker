059440cfcfd85f36fca21db041b3c8f48a08f9ec
Vasily Shuklin <b88bb@yandex.ru>
2026-06-14 18:22:24 +0500

    Feature: Extract PegChooser component for better separation of concerns
    - Create new PegChooser component to encapsulate color selection UI
    - Move peg selection logic from CodeBreakerView to dedicated component
    - Use callback pattern (onChoose) for communication between components
    - Simplify CodeBreakerView by removing inline pegChooser implementation

---
+++ CodeBreaker/View/PegChooser.swift
@@ -0,0 +1,18 @@
+//
+//  PegChooser.swift
+//  CodeBreaker
+//
+//  Created by Vasily_1 on 14.06.2026.
+//
+
+import SwiftUI
+
+struct PegChooser: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PegChooser()
+}
